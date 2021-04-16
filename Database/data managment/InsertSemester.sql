d3019a8634001a2664008d74fc525303485d5626
vip30service123 <80677107+vip30service123@users.noreply.github.com>
2021-04-16 23:34:08 +0700

    Update InsertSemester.sql

--- Database/data managment/InsertSemester.sql
+++ Database/data managment/InsertSemester.sql
@@ -1,4 +1,12 @@
 CREATE DEFINER=`root`@`localhost` PROCEDURE `InsertSemester`(SCode VARCHAR(6), SName VARCHAR(50), AYCode VARCHAR(6))
 BEGIN
-    INSERT INTO program (SCode, SName, AYName) VALUES (SCode, SName, AYName);
+	DECLARE flag INT;
+    SET flag=0;
+	SET flag =(SELECT SUM(CheckSemester(SCode, SName, AYCode)) FROM semester);
+    IF flag=0 THEN
+		SELECT 'Success';
+		INSERT INTO semester (SCode, SName, AYCode) VALUES (SCode, SName, AYCode);
+	ELSE 
+		SELECT 'Duplicate';
+    END IF;
 END
