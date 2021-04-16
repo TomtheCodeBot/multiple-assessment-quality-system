30d6c43fcdeec36d1037057e2c0861029e8f3a32
vip30service123 <80677107+vip30service123@users.noreply.github.com>
2021-04-16 22:12:40 +0700

    Update CheckLogin.sql

--- Database/RBAC/CheckLogin.sql
+++ Database/RBAC/CheckLogin.sql
@@ -1,8 +1,8 @@
-CREATE DEFINER=`root`@`localhost` FUNCTION `CheckLogin`(Login VARCHAR(50)) RETURNS int
+CREATE DEFINER=`root`@`localhost` FUNCTION `CheckLogin`(Login VARCHAR(20), Pass VARCHAR(20)) RETURNS int
 BEGIN
 	DECLARE flag INT;
     SET flag = 0;
-    IF (Login) IN (SELECT Login FROM account)
+    IF (Login, Pass) IN (SELECT A.Login, A.Pass FROM account A)
     THEN SET flag=1;
     END IF;
 RETURN flag;
