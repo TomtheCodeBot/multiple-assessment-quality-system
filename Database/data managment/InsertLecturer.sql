275022e3b81600d1a98051744d78980a6eb49155
vip30service123 <80677107+vip30service123@users.noreply.github.com>
2021-04-16 23:32:50 +0700

    Update InsertLecturer.sql

--- Database/data managment/InsertLecturer.sql
+++ Database/data managment/InsertLecturer.sql
@@ -1,4 +1,12 @@
 CREATE DEFINER=`root`@`localhost` PROCEDURE `InsertLecturer`(LCode VARCHAR(6), LName VARCHAR(50), CCode VARCHAR(6))
 BEGIN
-    INSERT INTO lecturer (LCode, LName, CCode) VALUES (LCode, LName, CCode);
+	DECLARE flag INT;
+    SET flag=0;
+	SET flag =(SELECT SUM(CheckLecturerToInsert(LCode, LName, CCode)) FROM lecturer);
+    IF flag=0 THEN
+		SELECT 'Success';
+		INSERT INTO lecturer (LCode, LName, CCode) VALUES (LCode, LName, CCode);
+	ELSE 
+		SELECT 'Duplicate';
+    END IF;
 END
