112d656ee4ee9afc9c984cbec36cb584924fb709
TomtheCodeBot <65719931+TomtheCodeBot@users.noreply.github.com>
2021-04-17 13:21:02 +0700

    Update CheckLecturer.sql

--- Database/RBAC/CheckLecturer.sql
+++ Database/RBAC/CheckLecturer.sql
@@ -1,25 +1,13 @@
 
 CREATE DEFINER=`root`@`localhost` PROCEDURE `CheckLecturer`(Login VARCHAR(20))
 BEGIN
-DECLARE lecturer varchar(20);    
-  SET lecturer = NULL;
-	IF (Login) IN (SELECT A.Login 
-		       FROM user U 
-		       		NATURAL JOIN lecturer L 
-		       		NATURAL JOIN account A 
-		       		NATURAL JOIN class C
-		       WHERE L.LName = U.Username 
-		       		AND A.Login = U.Login
-		      		AND L.CCode = C.CCode)
-	THEN SET lecturer = (SELECT L.LName 
-			 FROM user U 
-			 	NATURAL JOIN lecturer L 
-			 	NATURAL JOIN account A 
-			     	NATURAL JOIN class C
-			 WHERE L.LName = U.Username 
+SELECT C.CName 
+		FROM user U 
+		NATURAL JOIN lecturer L 
+		NATURAL JOIN account A 
+		NATURAL JOIN class C
+		WHERE L.LName = U.Username 
 			 	AND A.Login = U.Login 
 			 	AND A.Login = Login
-			    	AND L.CCode = C.CCode);
-	END IF;
-	SELECT lecturer;
+			    	AND L.CCode = C.CCode;
 END
