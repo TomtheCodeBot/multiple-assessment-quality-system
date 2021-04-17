7710a6ef1e0fbe5d1916c5baf9936e6f0bec6b1f
vip30service123 <80677107+vip30service123@users.noreply.github.com>
2021-04-17 12:24:26 +0700

    Update CheckLecturer.sql

--- Database/RBAC/CheckLecturer.sql
+++ Database/RBAC/CheckLecturer.sql
@@ -7,15 +7,19 @@ DECLARE lecturer varchar(20);
 		       FROM user U 
 		       		NATURAL JOIN lecturer L 
 		       		NATURAL JOIN account A 
+		       		NATURAL JOIN class C
 		       WHERE L.LName = U.Username 
-		       		AND A.Login = U.Login)
+		       		AND A.Login = U.Login
+		      		AND L.CCode = C.CCode)
 	THEN SET lecturer = (SELECT L.LName 
 			 FROM user U 
 			 	NATURAL JOIN lecturer L 
 			 	NATURAL JOIN account A 
+			     	NATURAL JOIN class C
 			 WHERE L.LName = U.Username 
 			 	AND A.Login = U.Login 
-			 	AND A.Login = Login);
+			 	AND A.Login = Login
+			    	AND L.CCode = C.CCode);
 	END IF;
 	SELECT lecturer;
 END
