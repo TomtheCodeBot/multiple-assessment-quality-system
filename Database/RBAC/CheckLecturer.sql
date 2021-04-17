4de9a7f277f4bcc362ee4e0bcdc5c77ebf801a47
Truong Duong Le <53457340+duongross@users.noreply.github.com>
2021-04-17 09:15:33 +0700

    Dont have flag

--- Database/RBAC/CheckLecturer.sql
+++ Database/RBAC/CheckLecturer.sql
@@ -9,7 +9,7 @@ DECLARE lecturer varchar(20);
 		       		NATURAL JOIN account A 
 		       WHERE L.LName = U.Username 
 		       		AND A.Login = U.Login)
-	THEN SET flag = (SELECT L.LName 
+	THEN SET lecturer = (SELECT L.LName 
 			 FROM user U 
 			 	NATURAL JOIN lecturer L 
 			 	NATURAL JOIN account A 
