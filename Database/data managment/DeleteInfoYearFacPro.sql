fcb513a36d2264929d79d23857b7029e32ab0c35
vip30service123 <80677107+vip30service123@users.noreply.github.com>
2021-04-19 21:22:36 +0700

    Update DeleteInfoYearFacPro.sql

--- Database/data managment/DeleteInfoYearFacPro.sql
+++ Database/data managment/DeleteInfoYearFacPro.sql
@@ -1,5 +1,11 @@
 CREATE DEFINER=`root`@`localhost` PROCEDURE `DeleteInfoYearFacPro`(pro_fac_code VARCHAR(6))
 BEGIN
+	DECLARE flag INT;
+    DECLARE size1 INT;
+    SET size1 = (SELECT COUNT(*) FROM aca_fac_pro);
 	DELETE FROM aca_fac_pro 
 	WHERE PFCode = pro_fac_code;
+    IF size1 = (SELECT COUNT(*) FROM aca_fac_pro) THEN SELECT 0;
+    ELSE SELECT 1;
+    END IF;
 END
