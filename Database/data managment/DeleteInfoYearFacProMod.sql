afb655f40f3453352c35562444e17ccf577115fd
vip30service123 <80677107+vip30service123@users.noreply.github.com>
2021-04-19 20:58:56 +0700

    Update DeleteInfoYearFacProMod.sql

--- Database/data managment/DeleteInfoYearFacProMod.sql
+++ Database/data managment/DeleteInfoYearFacProMod.sql
@@ -1,6 +1,6 @@
 CREATE DEFINER=`root`@`localhost` PROCEDURE `DeleteInfoYearFacProMod`(pro_fac_code VARCHAR(6), module_code VARCHAR(6))
 BEGIN
 	DELETE FROM aca_pro_mod 
- 	WHERE PFCode LIKE pro_fac_code
-	AND MCode LIKE module_code;
+ 	WHERE PFCode = pro_fac_code
+	AND MCode = module_code;
 END
