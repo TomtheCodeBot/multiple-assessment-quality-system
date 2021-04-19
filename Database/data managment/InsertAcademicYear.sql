e976486927c8f00cd7c998b886e0177f9660db6b
vip30service123 <80677107+vip30service123@users.noreply.github.com>
2021-04-20 00:04:40 +0700

    Update InsertAcademicYear.sql

--- Database/data managment/InsertAcademicYear.sql
+++ Database/data managment/InsertAcademicYear.sql
@@ -4,6 +4,10 @@ BEGIN
     DECLARE a INT;
     SET a = 0;
     SET AYCode = concat(floor(rand()*1000000));
+    WHILE AYCode IN (SELECT A.AYCode FROM academic_year A)
+		DO
+			SET AYCode = concat(floor(rand()*1000000));
+		END WHILE;
 	IF AYName NOT IN (SELECT A.AYName FROM academic_year A)
     THEN IF AYCode NOT IN (SELECT A.AYCode FROM academic_year A) 
 		THEN INSERT INTO academic_year VALUES (AYCode, AYName);
