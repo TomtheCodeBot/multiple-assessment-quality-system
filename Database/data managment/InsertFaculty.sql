a66b9652d1f52522836fe741a298f31bb01d10ac
vip30service123 <80677107+vip30service123@users.noreply.github.com>
2021-04-19 23:35:44 +0700

    Update InsertFaculty.sql

--- Database/data managment/InsertFaculty.sql
+++ Database/data managment/InsertFaculty.sql
@@ -1,12 +1,14 @@
-CREATE DEFINER=`root`@`localhost` PROCEDURE `InsertFaculty`(FCode VARCHAR(6), FName VARCHAR(50))
+CREATE DEFINER=`root`@`localhost` PROCEDURE `InsertFaculty`(FName VARCHAR(50))
 BEGIN
-	DECLARE flag INT;
-    SET flag=0;
-	SET flag =(SELECT SUM(CheckFaculty(FCode, FName)) FROM faculty);
-    IF flag=0 THEN
-		SELECT 'Success';
-		INSERT INTO faculty (FCode, FName) VALUES (FCode, FName);
-	ELSE 
-		SELECT 'Duplicate';
-    END IF;
+	DECLARE FCode VARCHAR(6);
+    DECLARE a INT;
+    SET a = 0;
+    SET FCode = concat(floor(rand()*1000000));
+	IF FName NOT IN (SELECT F.FName FROM faculty F)
+    THEN IF FCode NOT IN (SELECT F.FCode FROM faculty F) 
+		THEN INSERT INTO faculty VALUES (FCode, FName);
+        SET a = 1;
+		END IF;
+	END IF;
+    SELECT a;
 END
