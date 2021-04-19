385df4e3bb7f5d0f9ef15cfefb96e4e14025141d
vip30service123 <80677107+vip30service123@users.noreply.github.com>
2021-04-20 00:24:35 +0700

    Update InsertLecturer.sql

--- Database/data managment/InsertLecturer.sql
+++ Database/data managment/InsertLecturer.sql
@@ -3,8 +3,12 @@ BEGIN
 	DECLARE LCode VARCHAR(6);
     DECLARE a INT;
     SET a = 0;
-    SET LCode = concat(floor(rand()*1000000));
-	IF (LName, CCode) NOT IN (SELECT L.FName, L.CCode FROM lecturer L)
+    SET LCode = concat('L', floor(rand()*100000));
+    WHILE LCode IN (SELECT L.LCode FROM lecturer L)
+		DO
+			SET LCode = concat('L', floor(rand()*100000));
+		END WHILE;
+	IF (LName, CCode) NOT IN (SELECT L.LName, L.CCode FROM lecturer L)
     THEN IF LCode NOT IN (SELECT L.LCode FROM lecturer L) 
 		THEN INSERT INTO lecturer VALUES (LCode, LName, CCode);
         SET a = 1;
