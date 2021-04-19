3a7f1e843228822045c12c7b21bc7f9fe33d9954
TomtheCodeBot <65719931+TomtheCodeBot@users.noreply.github.com>
2021-04-20 00:19:03 +0700

    Update DeleteInfoYearFacPro.sql

--- Database/data managment/DeleteInfoYearFacPro.sql
+++ Database/data managment/DeleteInfoYearFacPro.sql
@@ -1,13 +1,9 @@
-CREATE DEFINER=`root`@`localhost` PROCEDURE `DeleteInfoYearFacPro`(academic_year_code VARCHAR(6), faculty_code VARCHAR(6), program_code VARCHAR(7))
+CREATE DEFINER=`root`@`localhost` PROCEDURE `DeleteInfoYearFacPro`(pro_fac_code)
 BEGIN
-	IF ((academic_year_code,faculty_code,program_code) IN (SELECT AYCode,FCode,PCode FROM aca_fac_pro NATURAL JOIN aca_faculty)) THEN
+	IF ((pro_fac_code) IN (SELECT PFCode FROM aca_fac_pro)) THEN
 		SELECT '1';
 		DELETE FROM aca_fac_pro 
-		WHERE AFCode LIKE (SELECT A.AFCode
-							FROM aca_faculty A
-							WHERE A.AYCODE LIKE academic_year_code 
-							AND A.FCode LIKE faculty_code)
-		AND PCode LIKE program_code;
+		WHERE pro_fac_code=PFCode;
 	ELSE
 		SELECT '0';
 	END IF;
