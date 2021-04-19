cf862c45487a72ceed48bfae91dcb6e27548b0e0
TomtheCodeBot <65719931+TomtheCodeBot@users.noreply.github.com>
2021-04-20 02:04:03 +0700

    Update DeleteInfoYearFac.sql

--- Database/data managment/DeleteInfoYearFac.sql
+++ Database/data managment/DeleteInfoYearFac.sql
@@ -1,10 +1,10 @@
-CREATE DEFINER=`root`@`localhost` PROCEDURE `DeleteInfoYearFac`(academic_year_code VARCHAR(6), faculty_code VARCHAR(6))
+CREATE DEFINER=`root`@`localhost` PROCEDURE `DeleteInfoYearFac`(aca_fac_code VARCHAR(6))
 BEGIN
-	IF ((academic_year_code,faculty_code) IN (SELECT AYCode,FCode FROM aca_faculty)) THEN
-		SELECT '1';
-		DELETE FROM aca_faculty WHERE AYCode = academic_year_code AND FCode = faculty_code;
-    ELSE
-		SELECT '0';
+	DECLARE a INT;
+    SET a = 0;
+	IF ((aca_fac_code) IN (SELECT AFCode FROM aca_faculty)) THEN
+		SET a= 1;
+		DELETE FROM aca_faculty WHERE AFCode=aca_fac_code;
     END IF;
-	
+	SELECT a;
 END
