b3800e8ebb481235d11b3602c35b4189fcd5ff41
TomtheCodeBot <65719931+TomtheCodeBot@users.noreply.github.com>
2021-04-20 02:07:11 +0700

    Update DeleteInfoYearFacPro.sql

--- Database/data managment/DeleteInfoYearFacPro.sql
+++ Database/data managment/DeleteInfoYearFacPro.sql
@@ -1,10 +1,11 @@
 CREATE DEFINER=`root`@`localhost` PROCEDURE `DeleteInfoYearFacPro`(pro_fac_code VARCHAR(6))
 BEGIN
+	DECLARE a INT;
+    SET a = 0;
 	IF ((pro_fac_code) IN (SELECT PFCode FROM aca_fac_pro)) THEN
-		SELECT '1';
+		SET a=1;
 		DELETE FROM aca_fac_pro 
 		WHERE PFCode=pro_fac_code;
-	ELSE
-		SELECT '0';
 	END IF;
+    SELECT a;
 END
