e81ef554d8ca0cc30bdca071326c268ba1b4cfda
TomtheCodeBot <65719931+TomtheCodeBot@users.noreply.github.com>
2021-04-21 12:24:23 +0700

    Update InsertClass.sql

--- Database/data managment/InsertClass.sql
+++ Database/data managment/InsertClass.sql
@@ -8,11 +8,19 @@ BEGIN
 		DO
 			SET CCode = concat('C', floor(rand()*100000));
 		END WHILE;
-	IF MCode IN (SELECT M.MCode FROM module M)
-		THEN IF SCode IN (SELECT S.SCode FROM semester S) 
-			THEN IF CName NOT IN (SELECT C.CName FROM class C)
+	IF (MCode,CCode) IN (SELECT MCode,CCode FROM  Aca_Faculty
+		NATURAL JOIN Academic_year
+		NATURAL JOIN Lecturer
+		NATURAL JOIN Faculty 
+		NATURAL JOIN aca_fac_pro
+		NATURAL JOIN Program
+		NATURAL JOIN aca_pro_mod
+		NATURAL JOIN Module
+		NATURAL JOIN Semester  )
+		THEN IF CName NOT IN (SELECT C.CName FROM class C)
+			THEN IF (MCode,CCode) NOT IN(SELECT MCode,CCode FROM class)
 				THEN INSERT INTO class VALUES (CCode, CName, Size, MCode, SCode);
-                SET a = 1;
+					SET a = 1;
 			END IF;
 		END IF;
 	END IF;
