65d4c3716c8e9d8e041c71333cc3bb766994eeb1
TomtheCodeBot <65719931+TomtheCodeBot@users.noreply.github.com>
2021-04-21 12:25:21 +0700

    Update InsertAcademicYear.sql

--- Database/data managment/InsertAcademicYear.sql
+++ Database/data managment/InsertAcademicYear.sql
@@ -3,12 +3,12 @@ BEGIN
 	DECLARE AYCode VARCHAR(6);
     DECLARE a INT;
     SET a = 0;
-    SET AYCode = concat('A', floor(rand()*100000));
+    SET AYCode = concat('A',floor(rand()*100000));
     WHILE AYCode IN (SELECT A.AYCode FROM academic_year A)
 		DO
-			SET AYCode = concat('A', floor(rand()*100000));
+			SET AYCode = concat('A',floor(rand()*100000));
 		END WHILE;
-	IF AYName NOT IN (SELECT A.AYName FROM academic_year A)
+	IF AYName NOT IN (SELECT A.AYName FROM academic_year A) AND concat('',AYName * 1) = AYName
     THEN IF AYCode NOT IN (SELECT A.AYCode FROM academic_year A) 
 		THEN INSERT INTO academic_year VALUES (AYCode, AYName);
         SET a = 1;
