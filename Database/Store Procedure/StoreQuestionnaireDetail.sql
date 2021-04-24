bf864e6f42c21bca41d6d855023fec092a11c752
Truong Duong Le <53457340+duongross@users.noreply.github.com>
2021-04-24 19:26:52 +0700

    Update StoreQuestionnaireDetail.sql

--- Database/Store Procedure/StoreQuestionnaireDetail.sql
+++ Database/Store Procedure/StoreQuestionnaireDetail.sql
@@ -6,12 +6,12 @@ question_10 VARCHAR(10), question_11 VARCHAR(10), question_12 VARCHAR(10), quest
 question_14 VARCHAR(10), question_15 VARCHAR(10), question_16 VARCHAR(10), question_17 VARCHAR(10), 
 question_18 VARCHAR(500))
 BEGIN
-	declare
-    classcode varchar(10);
-    declare
-    lecturercode varchar(10);
-	set classcode=(SELECT CCode from class where CName=classname);
-	set lecturercode=(SELECT LCode from lecturer where LName=lecturername and CCode=classcode);
+
+    declare classcode varchar(10);
+    declare lecturercode varchar(10);
+    set classcode=(SELECT CCode from class where CName=classname);
+    set lecturercode=(SELECT LCode from lecturer where LName=lecturername and CCode=classcode);
+		    
     INSERT INTO questionnaire (CCode, LCode, attend, gender, Q1, Q2, Q3, Q4, Q5, Q6, Q7, Q8, Q9, Q10, 
     Q11, Q12, Q13, Q14, Q15, Q16, Q17, Q18) 
     VALUES(classcode, lecturercode, attendance, gender, question_1, question_2, question_3, question_4, question_5, 
